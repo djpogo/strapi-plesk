5ebae017b11fcf5e15c1556e47a8ef9835c39c1e
Raoul <git@raoulkramer.de>
2020-08-22 20:27:29 +0200

    initial strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '87a0d240c3557275a3bae13c5273c181'),
+    },
+  },
+});
